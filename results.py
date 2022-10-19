7980273ca65a4cd171ce3f531496cd9b1e28ff59
Muennighoff <n.muennighoff@gmail.com>
2022-10-19 15:51:46 +0200

    Update configs

--- results.py
+++ results.py
@@ -53,7 +53,10 @@ MODELS = [
     "sgpt-bloom-7b1-msmarco",
     "sup-simcse-bert-base-uncased",
     "text-similarity-ada-001",
-    "text-search-ada-query-001",
+    "text-search-ada-001",
+    "text-search-babbage-001",
+    "text-search-curie-001",
+    "text-search-davinci-001",             
     "text-search-ada-doc-001",
     "unsup-simcse-bert-base-uncased",
 ]
