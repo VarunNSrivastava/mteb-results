61f1703ac3b9b247a0920c0d886f2cdd457d0a33
Niklas Muennighoff <Muennighoff@users.noreply.huggingface.co>
2022-10-20 08:29:34 +0000

    Fix name

--- results.py
+++ results.py
@@ -10,9 +10,10 @@ logger = datasets.logging.get_logger(__name__)
 _CITATION = """@article{muennighoff2022mteb,
   doi = {10.48550/ARXIV.2210.07316},
   url = {https://arxiv.org/abs/2210.07316},
-  author = {Muennighoff, Niklas and Tazi, Nouamane and Magne, Loïc and Reimers, Nils},
+  author = {Muennighoff, Niklas and Tazi, Nouamane and Magne, Lo{\"\i}c and Reimers, Nils},
   title = {MTEB: Massive Text Embedding Benchmark},
   publisher = {arXiv},
+  journal={arXiv preprint arXiv:2210.07316},  
   year = {2022}
 }
 """
